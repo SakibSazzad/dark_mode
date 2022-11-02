a30837560a593c740baa785120e798e4cd1438cb
SakibSazzad <saxibsazzad@gmail.com>
2022-11-02 12:31:50 -0700

    jquery add

--- js/app.js
+++ js/app.js
@@ -1,33 +1,53 @@
-var password = document.getElementById("password");
-var password_eye = document.getElementById("password_eye");
-var modeicon = document.getElementById("mode");
-var toggle_bar = document.getElementById("menu_toggle_bar");
+$(function () {
+  // password eye button start
+  $("#password_eye").on("click", function () {
+    if (password.type === "password") {
+      password.type = "text";
+      password_eye.addClass("fa-eye-slash");
+      password_eye.removeClass("fa-eye");
+    } else {
+      password.type = "password";
+      password_eye.removeClass("fa-eye-slash");
+      password_eye.addClass("fa-eye");
+    }
+  });
+  // password eye button End
 
-password_eye.addEventListener("click", function () {
-  if (password.type === "password") {
-    password.type = "text";
-    password_eye.classList.add("fa-eye");
-    password_eye.classList.remove("fa-eye-slash");
-  } else {
-    password.type = "password";
-    password_eye.classList.add("fa-eye-slash");
-    password_eye.classList.remove("fa-eye");
-  }
+  // Darkmode icon start
+  $("#mode").on("click", function () {
+    $("body").toggleClass("darkmode");
+
+    if ($("#mode").hasClass("fa-solid")) {
+      $("#mode").addClass("fa-regular");
+      $("#mode").removeClass("fa-solid");
+    } else {
+      $("#mode").addClass("fa-solid");
+      $("#mode").removeClass("fa-regular");
+    }
+  });
+  // Darkmode icon End
+
+  // menu toggle bar start
+  $("#menu_toggle_bar").on("click", function () {
+    $("#sidemenu").toggleClass("active");
+  });
+  // menu toggle bar End
+
+  // nav fixed scroll start
+  $(window).on("scroll", function () {
+    var scrollsize = $(window).scrollTop();
+
+    if (scrollsize > 136) {
+      $("#nav").addClass("active");
+    } else {
+      $("#nav").removeClass("active");
+    }
+  });
+  // nav fixed scroll End
+
+  // Preloader start
+  $(window).on("load", function () {
+    $("#preloader").fadeOut(1000);
+  });
+  // Preloader End
 });
-
-modeicon.addEventListener("click", function () {
-  document.body.classList.toggle("darkmode");
-
-  if (modeicon.classList.contains("fa-solid")) {
-    modeicon.classList.add("fa-regular");
-    modeicon.classList.remove("fa-solid");
-  } else {
-    modeicon.classList.remove("fa-regular");
-    modeicon.classList.add("fa-solid");
-  }
-});
-
-toggle_bar.addEventListener("click", function () {
-  document.getElementById("sidemenu").classList.toggle("active");
-});
-
